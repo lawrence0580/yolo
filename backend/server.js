ff5ac0d83edd473f22e52087d459f9496d9c656b
Lawrence <larryitotia@gmail.com>
2022-12-08 21:57:56 +0300

    added mongo db url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb+srv://laki:laki123@cluster0.3vr4shl.mongodb.net/?retryWrites=true&w=majority';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
